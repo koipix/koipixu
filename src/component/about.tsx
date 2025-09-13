4362406e54656b8e9476e9369b066e257c07588a
Koipix <koi.devt@gmail.com>
2025-09-13 10:59:30 +0800

    specs/blog update

--- src/component/about.tsx
+++ src/component/about.tsx
@@ -1,7 +1,7 @@
 function About() {
   return (
     <>
-      <p className="text-gray-400 text-lg mb-5">Last Update: May 5, 2025 &lt;3</p>
+      <p className="text-gray-400 text-lg mb-5">Last Update: Sep 13, 2025 &lt;3</p>
       <img className="rounded-xl" src="/imgs/kana-bg.png" alt="konata"/>
       <h1 className="text-4xl font-bold mb-15 mt-5 text-center">よろしくね〜！ </h1>
 
@@ -10,22 +10,22 @@ function About() {
       <h1 className="text-3xl mt-13 border-b-2 border-dashed pb-3">@misystewmowo</h1>
 
       <p className="mt-7 text-xl font-normal">Operating System: <span className="text-sky-400 font-semibold">Arch Linux x86_64</span></p>
-      <p className="text-xl font-normal">Desktop Enviroment: <span className="text-rose-400 font-semibold">KDE Plasma</span></p>
+      <p className="text-xl font-normal">Desktop Enviroment: <span className="text-rose-400 font-semibold">Hyprland</span></p>
       <p className="text-xl font-normal">Code Editor: <span className="text-lime-400 font-semibold">Neovim</span></p>
       <p className="text-xl font-normal mb-5">Shell: <span className="text-violet-400 font-semibold">Bash</span></p>
-      <p className="text-xl font-normal">CPU: <span className="text-rose-500 font-semibold">AMD Ryzen 5 3600</span></p>
+      <p className="text-xl font-normal">CPU: <span className="text-rose-500 font-semibold">AMD Ryzen 5 5600X</span></p>
       <p className="text-xl font-normal">GPU: <span className="text-rose-500 font-semibold">AMD Radeon RX 5700 XT</span></p>
       <p className="text-xl font-normal pb-7 border-b-2 border-dashed">Display: <span className="text-rose-500 font-semibold">1920x1080 @ 165Hz</span></p>
       
     <h1 className="text-3xl mt-15">@backstowy</h1>
-    <p className="text-2xl/8 font-normal mt-6">I'm always enthusiastic about the constantly growing technologies and genuinely craving them during my leisure time with a little dip of anime and manga!</p>
+    <p className="text-2xl/8 font-normal mt-6">I'm always enthusiastic about the constantly growing technologies and genuinely craving them during my leisure time with a little dip of anime and manga at times (as an excuse for procastinating).</p>
 
     <p className="text-2xl/8 font-normal mt-10"> I used to be the long lost one fellow during my childhood years until my parents introduced me to my first PC where I'd just play a few games until hours on end. Here's where my young mind started being curious on how these games actually work behind the scenes.
     </p>
     <p className="text-2xl/8 font-normal mt-10">So, I started digging deeper and discovered programming where everything looked like an alien scripture for me so I studied how computer components work with their own responsibilites and a ton of research hence I fell to this inescapable rabbit hole which led me here to this present day~
     </p>
     <p className="text-2xl/8 font-normal mt-10">
-      Presently, I've been working on small scale projects while taking Bachelor's degree in Computer Science to acquire knowledge better and make something for fun out of myself especially game development which is currently favorable for me. Been doing a bit of research on LLMs and I also draw digital anime artstyle too!  
+      Presently, I've been working on small scale projects while taking Bachelor's degree in Computer Science to acquire knowledge better and make something for fun out of myself especially game development which is currently favorable for me. Been doing a bit of research on LLMs and I also draw during my leisure time too!  
     </p>
     <p className="text-2xl/8 font-normal mt-10">
       Thank you for reading my silly past time &lt;3
